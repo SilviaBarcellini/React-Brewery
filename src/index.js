f88ea0a8d44f458956df8b3aa0db2c70fd4958f4
SilviaBarcellini <77615477+SilviaBarcellini@users.noreply.github.com>
2021-05-20 19:13:50 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+//just one default export per file
+//more than one non-default export per file!!
+//split up file in different file components
+//modern version of require()
+//import * = import everything as ...
